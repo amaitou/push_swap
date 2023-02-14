997012e01171447f52a22c6fbfa013f0ca529f08
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-02-14 03:42:27 +0100

    fixed the int cheker

---
+++ ft_atoi.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+static int	contain(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		++s;
+	}
+	return (0);
+}
+
+static int	spaces(char c)
+{
+	return (contain("\t\n\v\f\r ", c));
+}
+
+static int	signs(char c)
+{
+	return (contain("-+", c));
+}
+
+int	ft_atoi(const char *str)
+{
+	char	*p;
+	int		s;
+	int		r;
+
+	p = (char *)str;
+	s = 1;
+	r = 0;
+	while (spaces(*p))
+		++p;
+	if (signs(*p))
+	{
+		if (*p == '-')
+			s *= -1;
+		++p;
+	}
+	while (*p >= '0' && *p <= '9')
+	{
+		r = r * 10 + *p - 48;
+		++p;
+	}
+	return (r * s);
+}
+
+int main(void)
+{
+	printf("%d", -0000001);
+	return (0);
+}
--- ft_checker.c
+++ ft_checker.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 09:10:11 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/02/12 12:46:17 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/02/14 03:38:36 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,24 @@ int check_duplicates(t_stack *st)
 	return (1);
 }
 
+int	int_count(long int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb <= 0)
+	{
+		nb *= (-1);
+		++i;
+	}
+	while (nb > 0)
+	{
+		++i;
+		nb /= 10;
+	}
+	return (i);
+}
+
 int check_range(t_stack *st)
 {
 	int	i;
@@ -39,7 +57,8 @@ int check_range(t_stack *st)
 	i = 0;
 	while (i < st->size)
 	{
-		if (st->arr[i] >= INT_MAX || st->arr[i] <= INT_MIN)
+		if ((st->arr[i] >= INT_MAX || st->arr[i] <= INT_MIN)
+			&& int_count(st->arr[i] >= 11))
 			return (0);
 		++i;
 	}
