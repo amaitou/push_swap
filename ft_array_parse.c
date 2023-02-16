c01a2e4dd2b9739728bd20e12361e043677bc31f
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-02-16 13:15:11 +0100

    still finding out a way to check the range of input values

--- ft_array_parse.c
+++ ft_array_parse.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 09:41:17 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/02/16 13:07:52 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/02/16 13:11:50 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,15 @@ void	ft_array_parse(t_stack *st, char c)
 
 	line = read_args(st);
 	p = ft_split(line, ' ');
-	size = array_size(p) - 1;
-	k = size;
+	size = array_size(p);
+	k = size - 1;
 	st->arr = (int *)malloc(sizeof(int) * size);
 	st->size = 0;
 	st->top = -1;
 	i = 0;
 	if (c == 'a')
 	{
-		while (i <= size)
+		while (i < size)
 		{
 			st->arr[i] = ft_3atwa(p[k]);
 			st->top++;
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 09:37:43 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/02/16 13:06:12 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/02/16 13:14:31 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	main(int ac, char **ag)
 		stb.input.ag = ag;
 		ft_array_parse(&sta, 'a');
 		ft_array_parse(&stb, 'b');
+		bubble_sort(&sta);
 		ft_perform_all(&sta, &stb);
 	}
 	return (0);
